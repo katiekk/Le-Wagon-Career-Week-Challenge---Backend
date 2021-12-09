f7e60f08fbefaa04dc87fd3bf3230c8249094b74
Katie Lin <katiekklht@gmail.com>
2021-12-09 18:05:49 +0000

    return result as JSON

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,13 +1,17 @@
 require 'json'
 require 'open-uri'
 
+
 class MuseumsController < ApplicationController
   def index
     if params[:lat].present?
-
+      url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museums.json?proximity=#{params[:lng]},#{params[:lat]}&access_token=#{ENV['MAPBOX_API_KEY']}"
+      museums = JSON.parse(URI.open(url).read)['features']
+      @museums_json = {}
+      museums.each do |museum|
+        @museums_json[museum['context'][0]['text']] = museum['text']
+      end
+      # redirect_to museums_path
     end
   end
 end
-
-base_url = "https://api.mapbox.com"
-endpoint_url = "/geocoding/v5/mapbox.places/museums.json"
